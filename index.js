31d4bae6ee94770d901ac6fbe4ffc69443c818d3
Erica Rojo <ericarojo692@yahoo.com>
2021-08-15 15:52:54 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    return drivers.filter(d => d.toLowerCase() === name.toLowerCase())
+}
+
+const fuzzyMatch = (drivers, name)=> {
+    return drivers.filter(d => name === d.slice(0,name.length))
+}
+
+
+const matchName = (drivers, name) => {
+    return drivers.filter(d => d.name === name)
+  }
+
+
+
